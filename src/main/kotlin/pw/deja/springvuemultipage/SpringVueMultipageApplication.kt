d512fab717283b29aee314384a5dff5e89666f2e
Klaus Deja <klaus.deja@gmail.com>
2019-11-24 02:09:43 +0100

    Renamed

---
+++ src/main/kotlin/pw/deja/springvuemultipage/SpringVueMultipageApplication.kt
@@ -0,0 +1,11 @@
+package pw.deja.springvuemultipage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringVueMultipageApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringVueMultipageApplication>(*args)
+}
